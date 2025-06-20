1f029f8eb1ddae75870cf9cde8386132753bd571
Milchschnittli <a1900939@adelaide.edu.au>
2025-06-20 14:46:51 +0000

    [automatic commit] 2025-06-20 14:46:51 UTC ;; communications error to 208.67.222.222#53: timed out 122.150.235.204

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const db = require('./db');
 
 const app = express();
 // Session Middleware requirement
