eebf420218bb23ef19a2132a812c3ca2a0263bc1
Milchschnittli <a1900939@adelaide.edu.au>
2025-06-20 15:36:21 +0000

    [automatic commit] 2025-06-20 15:36:21 UTC 122.150.235.170

--- part2/app.js
+++ part2/app.js
@@ -53,7 +53,6 @@ app.post('/login', async (req, res) => {
         }
             return res.json({ redirect: '/walker-dashboard.html' });
     } catch (err) {
-        console.error('Login route error:', err);
         res.status(500).json({ error: 'Login failed ' });
     }
 });
