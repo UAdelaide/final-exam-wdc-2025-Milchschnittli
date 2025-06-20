d31caf017ca8b607a1d249b353cf25e8a99bbd7c
Milchschnittli <a1900939@adelaide.edu.au>
2025-06-20 15:58:36 +0000

    [automatic commit] 2025-06-20 15:58:36 UTC 122.150.235.204

--- part2/app.js
+++ part2/app.js
@@ -31,7 +31,7 @@ app.get('/api/dogs', async (req, res) => {
         const [rows] = await pool.query(`
             SELECT
                 d.dog_id,
-                d.dog_name,
+                d.name AS dog_name,
                 d.size,
                 u.username AS owner_username
             FROM Dogs d
@@ -39,7 +39,7 @@ app.get('/api/dogs', async (req, res) => {
             `);
             res.json(rows);
     } catch (err) {
-        console.error('Error fetching all dogs:', err);
+
         res.status(500).json({ error: 'Failed to retrieve all dog data ' });
     }
 });
